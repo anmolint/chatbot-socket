554394ac67af2642ecc8d093cb68a9f482d5b343
anmolint <anmol@excellencetechnologies.in>
2022-03-23 13:28:39 +0530

    socket task fixes

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ const dbops =require('./models/index')
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const console = require("console");
 const io = new Server(server);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 io.on("connection", async(socket) => {
-     let msgHistory=await dbops.Messages.find({});
-     let messagespast = msgHistory.map(item=>item.message)
-     io.emit("connection",messagespast)
+    let msgHistory=await dbops.Messages.find({});
+      msgHistory = msgHistory.filter(el => el.name)
+     let messagespast =  msgHistory.map(item => {
+      const messageData = {};
+  
+      messageData.name = item.name;
+      messageData.message = item.message
+      return messageData;
+  })
+ io.emit("connection",messagespast)
   socket.on("chat message", async (msg) => {
     io.emit("chat message", msg);
-    await dbops.Messages.create({message:msg});
+    await dbops.Messages.create({
+      name:msg.namevalue,
+      message:msg.messages
+    });
   });
 });
 
